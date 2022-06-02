03c22e2c718bd787c4d33a433b5604a5993a213a
lisboamab <lisboamab@gmail.com>
2022-06-02 19:49:15 -0300

    correnção na importação da key

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from django1.key_secret.key_secret import secretkey
 from pathlib import Path
-from key_secret import secretkey
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
