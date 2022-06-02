d8acd40b2c406f65bbab79c2473d14f439806025
lisboamab <lisboamab@gmail.com>
2022-06-02 19:31:13 -0300

    escondedo a SECRET_KEY

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from key_secret import secretkey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*m8=dy39_!gsuiz0$*v+ieqrepypdsp=7%ezed02f3yfqc6e=('
+SECRET_KEY = secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
