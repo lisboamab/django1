c18cb54dbe16cbe7eaa5bc70841d157bd927cc0a
lisboamab <lisboamab@gmail.com>
2022-06-03 23:58:32 -0300

    correção na forma de chamar as funções das páginas

--- django1/djangocore/urls.py
+++ django1/djangocore/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import index, contato
 
 urlpatterns = [
-    path('', 'index.html'),
-    path('contato', 'contato.html'),
-    
+    path('', index),
+    path('contato', contato),
+
 ]
 
