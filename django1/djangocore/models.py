a5b95fdd5a7f2320fc8251811bf5703d94650865
lisboamab <lisboamab@gmail.com>
2022-06-15 20:07:37 -0300

    create __str__ function in models.py and conect in admin.py

--- django1/djangocore/models.py
+++ django1/djangocore/models.py
@@ -5,3 +5,6 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
     estoque = models.IntegerField('Quantidade em estoque')
+
+    def __str__(self) -> str:
+        return self.nome
